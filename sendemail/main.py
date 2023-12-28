bbf6a02a253b25122c025c2df760a258d87f7ee9
Shakhobiddin Bozorov <102373978+Robben1972@users.noreply.github.com>
2023-12-28 23:09:07 +0500

    Add files via upload

---
+++ sendemail/main.py
@@ -0,0 +1,27 @@
+from signedup import signedupdef
+from signin import signindef
+
+take = 0
+while True:
+    print("1. Sign in")
+    print("2. Sign up")
+    print("3. Exit")
+    try:
+        section = int(input("Choose: "))
+        take = section
+    except ValueError:
+        print("Choose again")
+    if 3 >= take >= 1:
+        if take == 1:
+            signindef()
+        elif take == 2:
+            signedupdef()
+        elif take == 3:
+            break
+    else:
+        print("Choose again")
+
+        # "bozorovshahob27@gmail.com": "dapqtmlkkjfgljnb",
+
+
+
